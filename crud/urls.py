d1b37e25a9aece627f9cf78334d26435ce8b3c77
El profe <al.sepulvedam@profesor.duoc.cl>
2024-06-05 12:58:20 -0400

    LIstado listo

--- crud/urls.py
+++ crud/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, personas
+from .views import index, personas,pasadatos
 
 
 urlpatterns = [
     path('',index,name="index"),
-    path('personas/', personas, name='personas')
+    path('personas/', personas, name='personas'),
+    path('pasadatos/',pasadatos,name='pasadatos')
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from datetime import date, datetime
+from .models import Persona
 
 # Create your views here.
 def index(request):
@@ -6,4 +8,26 @@ def index(request):
 
 
 def personas(request):
-    return render(request,'crud/personas.html')
+    personas=Persona.objects.all()
+    
+    datos={
+        "personas":personas
+    }
+
+    return render(request,'crud/personas.html', datos)
+
+def pasadatos(request):
+    fecha=datetime.now()
+    mensaje="Mensaje desde la vista"
+    lista=["perro","gato","raton"]
+    numero=10
+    
+    datos={
+        "fecha":fecha,
+        "msg":mensaje,
+        "animales":lista,
+        "numero":numero
+    }
+    
+    print(f"La fecha de hoy es {fecha}")
+    return render(request,'crud/pasadatos.html', datos)
